b823c858e2d0eca4a7e2efa75af2ea7c7f2cf7a7
Hirozumi_Ryosuke <erpschrodinger@gmail.com>
2020-03-24 19:57:19 +0900

    2020/03/24 first

--- app/src/main/java/com/example/expandable_cardview_2/ExpandableCardViewAdapter.kt
+++ app/src/main/java/com/example/expandable_cardview_2/ExpandableCardViewAdapter.kt
@@ -6,15 +6,13 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.cardview_parent.view.*
 
-
-
 class ExpandableCardViewAdapter(var items: MutableList<Item>)
-    : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    : RecyclerView.Adapter<RecyclerView.viewHolder>() {
     companion object {
-        val PARENT = 0
-        val CHILD = 1
-        val OPEN = 0.0F
-        val CLOSE = 180.0F
+        const val PARENT = 0
+        const val CHILD = 1
+        const val OPEN = 0.0F
+        const val CLOSE = 180.0F
     }
 
     data class Item(val type: Int = 0,
@@ -22,12 +20,12 @@ class ExpandableCardViewAdapter(var items: MutableList<Item>)
                     var children: List<Item>? = null)
 
     inner class ItemHolder(v: View) : RecyclerView.ViewHolder(v) {
-        var textView = v.item_text
-        val toggleImageView = v.item_toggle_bottun
+        var textView = v.item_text!!
+        val toggleImageView = v.item_toggle_bottun!!
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val inflater = LayoutInflater.from(parent?.context)
+        val inflater = LayoutInflater.from(parent.context)
         var view: View? = null
 
         when (viewType) {
@@ -35,7 +33,7 @@ class ExpandableCardViewAdapter(var items: MutableList<Item>)
             CHILD -> view = inflater.inflate(R.layout.cardview_child, parent, false)
         }
 
-        return ItemHolder(view!!)
+        return this.ItemHolder(view!!)
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
@@ -43,32 +41,29 @@ class ExpandableCardViewAdapter(var items: MutableList<Item>)
         val item = items[position]
 
         itemHolder?.let {
-            it.toggleImageView?.let { it1 ->
+            it.toggleImageView.let { it1 ->
                 it1.setImageResource(R.drawable.toggle)
                 it1.rotation = if (item.children == null) OPEN else CLOSE
 
                 it1.setOnClickListener {
-                    val start = items.indexOf(item) + 1
+                    val start: Int = items.indexOf(item) + 1
                     if (item.children == null) {
                         var count = 0
                         var nextHeader = items.indexOf(items.find { it2 ->
                             (count++ >= start) && (it2.type == item.type)
                         })
 
-                        if (nextHeader == -1) {
-                            nextHeader = items.size
-                        }
+                        if (nextHeader == -1) nextHeader = items.size
                         item.children = items.slice(start until nextHeader)
 
                         val end = item.children!!.size
-                        if (end > 0) {
-                            item.removeAll(item.children!!)
-                        }
+                        if (end > 0) items.removeAll(item.children!!)
+
                         view.animate().rotation(CLOSE).start()
                         notifyItemRangeRemoved(start, end)
                     } else {
                         item.children?.let {
-                            this.items.addALL(start, it1)
+                            items.addALL(start, it1)
                             view.animate().rotation(OPEN).start()
                             notifyItemRangeInserted(start, it1.size)
                             item.children = null
@@ -81,11 +76,10 @@ class ExpandableCardViewAdapter(var items: MutableList<Item>)
         }
     }
 
-    override fun getItemCount(): Int {
-        return this.items.size
+
+    override fun getItemCount(): Int = items.size
+
+    override fun getItemViewType(position: Int): Int = items[position].type
     }
 
-    override fun getItemViewType(position: Int): Int {
-        return this.items[position].type
-    }
 }
