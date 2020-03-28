f2665b71fd19a55b22d42b9ad1ba72b0eba82caa
Hirozumi_Ryosuke <erpschrodinger@gmail.com>
2020-03-29 03:41:24 +0900

    2020/03/28 finished

--- app/src/main/java/com/example/expandable_cardview_2/MainActivity.kt
+++ app/src/main/java/com/example/expandable_cardview_2/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.example.expandable_cardview_2
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.cardview_child.*
+import kotlinx.android.synthetic.main.cardview_parent.*
+import android.view.inputmethod.InputMethodManager as InputMethodManager1
+import kotlinx.android.synthetic.main.cardview_parent.cardview_parent as cardview_parent1
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var inputMethodManager: InputMethodManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        item_text_parent = findViewById(R.id.cardview_parent)
+        item_text_child = findViewById(R.id.cardview_child)
+        InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
 
         val cardView= recycler_cardview
         val itemList = mutableListOf<ExpandableCardViewAdapter.Item>()
@@ -23,40 +35,14 @@ class MainActivity : AppCompatActivity() {
             ExpandableCardViewAdapter.Item(ExpandableCardViewAdapter.CHILD))
         itemList.add(item)
 
-        /*ExpandableCardViewAdapter.Item(ExpandableCardViewAdapter.PARENT)
-        item.children = listOf(
-            ExpandableCardViewAdapter.Item(ExpandableCardViewAdapter.CHILD),
-            ExpandableCardViewAdapter.Item(ExpandableCardViewAdapter.CHILD),
-            ExpandableCardViewAdapter.Item(ExpandableCardViewAdapter.CHILD),
-            ExpandableCardViewAdapter.Item(ExpandableCardViewAdapter.CHILD),
-            ExpandableCardViewAdapter.Item(ExpandableCardViewAdapter.CHILD))
-        itemList.add(item)*/
-
-
-        /*itemList.add(ExpandableCardViewAdapter.Item(ExpandableCardViewAdapter.PARENT))
-        itemList.add(ExpandableCardViewAdapter.Item(ExpandableCardViewAdapter.CHILD))
-        itemList.add(ExpandableCardViewAdapter.Item(ExpandableCardViewAdapter.CHILD))
-        itemList.add(ExpandableCardViewAdapter.Item(ExpandableCardViewAdapter.CHILD))
-        itemList.add(ExpandableCardViewAdapter.Item(ExpandableCardViewAdapter.CHILD))
-        itemList.add(ExpandableCardViewAdapter.Item(ExpandableCardViewAdapter.CHILD))*/
-
-        /*val nation = ExpandableCardViewAdapter.Item(ExpandableCardViewAdapter.PARENT, "Nation")
-        nation.children = listOf(
-            ExpandableCardViewAdapter.Item(ExpandableCardViewAdapter.CHILD, "Republic Of Korea"),
-            ExpandableCardViewAdapter.Item(ExpandableCardViewAdapter.CHILD, "Canada"),
-            ExpandableCardViewAdapter.Item(ExpandableCardViewAdapter.CHILD, "America"),
-            ExpandableCardViewAdapter.Item(ExpandableCardViewAdapter.CHILD, "Japan"),
-            ExpandableCardViewAdapter.Item(ExpandableCardViewAdapter.CHILD, "Denmark"),
-            ExpandableCardViewAdapter.Item(ExpandableCardViewAdapter.CHILD, "Britain"),
-            ExpandableCardViewAdapter.Item(ExpandableCardViewAdapter.CHILD, "China"),
-            ExpandableCardViewAdapter.Item(ExpandableCardViewAdapter.CHILD, "Russia"),
-            ExpandableCardViewAdapter.Item(ExpandableCardViewAdapter.CHILD, "France"),
-            ExpandableCardViewAdapter.Item(ExpandableCardViewAdapter.CHILD, "Turkey"))
-        itemList.add(nation)
-*/
-
-
         cardView.layoutManager = LinearLayoutManager(this)
         cardView.adapter = ExpandableCardViewAdapter(itemList)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        inputMethodManager.hideSoftInputFromWindow(cardview_parent1.windowToken, InputMethodManager1.HIDE_NOT_ALWAYS)
+        cardview_parent1.requestFocus()
+        cardview_child.requestFocus()
+        return false
     }
 }
